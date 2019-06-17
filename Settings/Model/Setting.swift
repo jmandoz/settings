3adda50141823a0f4c100913d78dbb481dffffde
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-06-17 10:59:42 -0600

    Completed Model

--- Settings/Model/Setting.swift
+++ Settings/Model/Setting.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2019 Jason Mandozzi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Setting {
+    let name: String
+    let icon: UIImage
+    var isOn: Bool
+    init(name: String, icon: UIImage, isOn: Bool) {
+        self.name = name
+        self.icon = icon
+        self.isOn = isOn
+    }
+}
