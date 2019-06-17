3a1260c8e0bdcaa84d83ec1eeb13fc1ee9d96215
Jason Mandozzi <jason.mandozzi@gmail.com>
2019-06-17 10:53:54 -0600

    Set up storyboard and file organization

---
+++ Settings/Contoller/SettingController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingController.swift
+//  Settings
+//
+//  Created by Jason Mandozzi on 6/17/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import Foundation
---
+++ Settings/Model/Setting.swift
@@ -0,0 +1,9 @@
+//
+//  Setting.swift
+//  Settings
+//
+//  Created by Jason Mandozzi on 6/17/19.
+//  Copyright © 2019 Jason Mandozzi. All rights reserved.
+//
+
+import Foundation
